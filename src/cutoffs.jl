961620ab8b8b2c691d8e0f679323adbad619fdf3
simonsteiger <simon.j.steiger@gmail.com>
2024-04-08 11:42:19 +0200

    add cut method for reals

--- src/cutoffs.jl
+++ src/cutoffs.jl
@@ -4,32 +4,48 @@ export cut
 
 using ..Composites
 
+# TODO document the cutoffs in some exported object (dict?)
+"Helper function containing the cutoffs for DAS28CRP"
+function _cutdas28crp(v::Real)
+    v < 2.5 ? "Remission" :
+    v ≤ 2.9 ? "Low" :
+    v ≤ 4.6 ? "Moderate" : "High"
+end
+
+"Helper function containing the cutoffs for DAS28ESR"
+function _cutdas28esr(v::Real)
+    v < 2.6 ? "Remission" :
+    v ≤ 3.2 ? "Low" :
+    v ≤ 5.1 ? "Moderate" : "High"
+end
+
 # TODO use composite and add conditional evaluation if value is missing
 """
     cut(c::AbstractDAS28)
 
-Convert a DAS28 score into a categorical variable.
+Convert the score of a DAS28 composite `c` into a categorical variable.
 
 ```julia-repl
 julia> x = DAS28ESR(4, 5, 12, 44)
 julia> cut(x)
 > "Moderate"
 ```
+
+    cut(v::Real, T::AbstractDAS28)
+
+Convert a DAS28 score `v` into a categorical variable according to the cutoffs for type `T`.
+
+```julia-repl
+julia> cut(5.4, DAS28CRP)
+> "High"
+```
 """
-function cut(c::AbstractDAS28)
-    value = score(c)
-    if c isa DAS28CRP
-        value < 2.5 ? "Remission" :
-        value ≤ 2.9 ? "Low"       :
-        value ≤ 4.6 ? "Moderate"  :
-                      "High"
-    else
-        value < 2.6 ? "Remission" :
-        value ≤ 3.2 ? "Low"       :
-        value ≤ 5.1 ? "Moderate"  :
-                      "High"
-    end
-end
+cut(c::AbstractDAS28) = c isa DAS28CRP ? _cutdas28crp(score(c)) : _cutdas28esr(score(c))
+cut(v::Real, ::Type{T}) where {T<:AbstractDAS28} = T == DAS28CRP ? _cutdas28crp(v) : _cutdas28esr(v)
+# TODO this dispatch could probably be much more general
+# It should first work on any AbstractComposite and then fork into AbstractDAS28
+# But we need an infrastructure that also elegantly handles, e.g., SDAI dispatch and is easily extensible beyond that
+# It could be nice to solve this with a function that takes a dict _cutwithdict and uses the dict dependent on which type was passed?
 
 """
     cut(c::SDAI)
@@ -44,10 +60,10 @@ julia> cut(x)
 """
 function cut(c::SDAI)
     value = score(c)
-    value <  3.3 ? "Remission" :
-    value ≤ 11.0 ? "Low"       :
-    value ≤ 26.0 ? "Moderate"  :
-                   "High" 
+    value < 3.3 ? "Remission" :
+    value ≤ 11.0 ? "Low" :
+    value ≤ 26.0 ? "Moderate" :
+    "High"
 end
 
 end
--- test/cutoffs.jl
+++ test/cutoffs.jl
@@ -1,5 +1,7 @@
 @testset "DAS28 cutoffs" begin
     @test cut(cDAS28ESR) == "Moderate"
+    @test cut(5.4, DAS28CRP) == "High"
+    @test cut(2.5, DAS28ESR) == "Remission"
 end
 
 @testset "SDAI cutoffs" begin
